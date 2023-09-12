266aca7f066f9b4711d3cd287ebdd1c397e2abbd
Jonas Steinmejer <jonas04@outlook.dk>
2023-09-12 10:24:43 +0200

    Created folders and edited Robots.cs

---
+++ Robots/Model/Robots.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots.Model
+{
+    internal class Robots
+    {
+        private bool isSmall;
+        private bool hasWifi;
+        private bool isWhite;
+    }
+}
